7d430f2b7c209c8e8272e591f9961297e7c3edb7
minxingjing <mxj12315@qq.com>
2023-07-23 14:54:38 +0800

    Rust

--- set/src/main.rs
+++ set/src/main.rs
@@ -1,3 +1,40 @@
+/*
+    set集合
+ */
+use std::collections::HashSet;
 fn main() {
-    println!("Hello, world!");
+    let mut st = HashSet::new();
+    st.insert("a");
+    st.insert("b");
+    st.insert("c");
+    println!("st={:?}",st);
+
+    // 遍历
+    for item in st.iter() {
+        println!("item={:?}",item);
+    }
+
+    // 删除元素
+    st.remove("a");
+    println!("st={:?}",st);
+
+    // 判断是否包含某个元素
+    let res = st.contains("b");
+    println!("res={:?}",res);
+
+    // 使用union方法合并两个集合
+    let mut st1 = HashSet::new();
+    st1.insert("a");
+    st1.insert("b");
+    st1.insert("c");
+    let mut st2 = HashSet::new();
+    st2.insert("a");
+    st2.insert("b");
+    st2.insert("d");
+    let st3 = st1.union(&st2);
+    println!("st3={:?}",st3); // {"a", "b", "c", "d"}
+
+    // 使用difference方法获取两个集合的差集
+    let st4 = st1.difference(&st2);
+    println!("st4={:?}",st4); // {"c"}
 }
