ff69c86c4686607b878f06b69f4ddd3097c1518a
minxingjing <mxj12315@qq.com>
2023-07-28 23:48:08 +0800

    Rust单元测试

--- UnitTest/src/lib.rs
+++ UnitTest/src/lib.rs
@@ -26,13 +26,24 @@ impl Rectangle {
     }
 }
 
+// 两个数相加
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+// 通过断言传入参数
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
 
 // 定义tests模块
 #[cfg(test)]
 mod tests {
-    //use super::*; 
+    //use super::*;
     // 也可以使用下面显示的引入
+    use super::add_two;
     use crate::Rectangle;
+    use super::greeting;
 
     #[test]
     fn larger_can_hold_smaller() {
@@ -46,5 +57,17 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2))
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let res = greeting("Test参数");
+        // 下面使用了断言参数，可以打印提示信息
+        assert!(res.contains("Ted"),"这是测试失败的提示信息:res =「{}」",res)
     }
 }
