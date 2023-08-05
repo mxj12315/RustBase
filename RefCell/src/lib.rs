57ec67640b022a741e9e77290df16b086da62b8e
minxingjing <mxj12315@qq.com>
2023-08-05 10:41:49 +0800

    Rust
    RefCell

--- RefCell/src/lib.rs
+++ RefCell/src/lib.rs
@@ -55,16 +55,20 @@ mod tests{
     }
 
     impl  Messenger for MockMessenger {
-        // send()方法内部会向vec数组中添加一个元素
+        // send()方法内部会向vec数组中添加一个元素,但是其参数是一个借用
+        // 外部值(self)被认为是不可变的情况下修改内部值(self.sent_messages)
         fn send(&self, msg: &str) {
-            self.sent_messages.borrow_mut().push(String::from(msg));
+            let mut one_borrow: std::cell::RefMut<Vec<String>> =  self.sent_messages.borrow_mut(); // 第一个可变借用
+            // one_borrow 和 two_borrow 都是同一个值的可变借用，不符合同一个作用域中一个变量只能有一个可变借用或者无数个不可变借用
+            // let mut two_borrow: std::cell::RefMut<Vec<String>> =  self.sent_messages.borrow_mut(); // 第二个可变借用
+            one_borrow.push(String::from(msg));
+            // two_borrow.push(String::from(msg));
         }
     }
 
     #[test]
     fn it_sends_an_over_75_percent_warning_message() {
         let mock_messenger: MockMessenger = MockMessenger::new();
-        mock_messenger.send("hello");
         let mut limit_tracker: LimitTracker<MockMessenger> = LimitTracker::new(&mock_messenger, 100);
         limit_tracker.set_value(80); // set_value方法的第一个参数类型&mut self,要求是一个可变的实例
         assert_eq!(mock_messenger.sent_messages.borrow().len(), 1); // 计算mock_messenger.sent_messages内vec数组的长度
---
+++ RefCell/target/debug/deps/RefCell-4f8aa16284ea2631.d
@@ -0,0 +1,5 @@
+/Users/min/Rust/RefCell/target/debug/deps/RefCell-4f8aa16284ea2631: src/main.rs
+
+/Users/min/Rust/RefCell/target/debug/deps/RefCell-4f8aa16284ea2631.d: src/main.rs
+
+src/main.rs:
